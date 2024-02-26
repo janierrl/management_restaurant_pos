0ee83145cf8237528cd90d8ef5f7feb7242f7d9b
janierrl <janierbits@yahoo.com>
2024-02-26 03:17:25 +0100

    add project

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,210 @@
+<template>
+    <div class="footer">
+        <div class="news-letter">
+            <h3>Recibe notificaciones de eventos</h3>
+            <form onsubmit="event.preventDefault();">
+                <input type="email" name="useremailreceiveinfo" placeholder="ingresa tu correo electrónico"
+                    id="useremailreceiveinfo">
+                <input type="submit" value="suscribirse">
+            </form>
+        </div>
+
+        <div class="box-container">
+
+            <div class="box">
+                <h3>nuestro menú</h3>
+                <router-link @click="scrollToTop()" to="/menu"><i class="fas fa-arrow-right"></i> taco</router-link>
+                <router-link @click="scrollToTop()" to="/menu"><i class="fas fa-arrow-right"></i> burrito</router-link>
+                <router-link @click="scrollToTop()" to="/menu"><i class="fas fa-arrow-right"></i> nachos</router-link>
+                <router-link @click="scrollToTop()" to="/menu"><i class="fas fa-arrow-right"></i> side food
+                </router-link>
+                <router-link @click="scrollToTop()" to="/menu"><i class="fas fa-arrow-right"></i> dessert</router-link>
+                <router-link @click="scrollToTop()" to="/menu"><i class="fas fa-arrow-right"></i> drink</router-link>
+            </div>
+
+            <div class="box">
+                <h3>enlaces rápidos</h3>
+                <router-link @click="scrollToTop()" to="/"> <i class="fas fa-arrow-right"></i> inicio</router-link>
+                <router-link @click="scrollToTop()" to="/about"> <i class="fas fa-arrow-right"></i> acerca de</router-link>
+                <router-link @click="scrollToTop()" to="/promotions"> <i class="fas fa-arrow-right"></i> promociones
+                </router-link>
+                <router-link @click="scrollToTop()" to="/menu"> <i class="fas fa-arrow-right"></i> menú</router-link>
+                <router-link @click="scrollToTop()" to="/table"> <i class="fas fa-arrow-right"></i> reservar una mesa
+                </router-link>
+            </div>
+
+            <div class="box">
+                <h3>enlaces adicionales</h3>
+                <div v-if="user">
+                    <router-link @click="scrollToTop()" to="/cart"> <i class="fas fa-arrow-right"></i> mi pedido
+                    </router-link>
+                    <router-link @click="scrollToTop()" to="/myorder"> <i class="fas fa-arrow-right"></i> mis pedidos
+                    </router-link>
+                </div>
+                <div v-else>
+                    <router-link @click="scrollToTop()" to="/login"> <i class="fas fa-arrow-right"></i> iniciar sesión
+                    </router-link>
+                    <router-link @click="scrollToTop()" to="/register"> <i class="fas fa-arrow-right"></i> registrarse
+                    </router-link>
+                </div>
+            </div>
+
+            <div class="box">
+                <h3>horario de apertura</h3>
+                <p>todos los días: 12:00 - 23:00</p>
+
+            </div>
+
+        </div>
+
+        <div class="bottom">
+
+            <div class="share">
+                <a href="https://www.facebook.com/restaurante.tanokura" target="_blank" class="fab fa-facebook-f"></a>
+                <a href="https://twitter.com/?lang=en" class="fab fa-twitter"></a>
+                <a href="https://www.instagram.com/restaurant_tanokura" target="_blank" class="fab fa-instagram"></a>
+                <a href="https://www.pinterest.com/" class="fab fa-pinterest"></a>
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+    name: 'FooterComponent',
+
+    computed: {
+        ...mapState(['user'])
+    },
+
+    methods: {
+        scrollToTop() {
+            window.scrollTo(0, 0);
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* footer */
+.footer {
+    background: #f7f7f7;
+    padding: 2rem 9%;
+}
+
+.footer .news-letter {
+    text-align: center;
+    margin-bottom: 2rem;
+}
+
+.footer .news-letter h3 {
+    font-size: 2.5rem;
+    color: #130f40;
+    padding-bottom: 1rem;
+}
+
+.footer .news-letter form {
+    max-width: 70rem;
+    margin: 1rem auto;
+    max-width: 70rem;
+
+    display: flex;
+    border-radius: .5rem;
+    overflow: hidden;
+}
+
+.footer .news-letter form input[type="email"] {
+    height: 100%;
+    width: 100%;
+    padding: 1rem 1.2rem;
+    font-size: 1.6rem;
+    color: #130f40;
+    text-transform: none;
+}
+
+.footer .news-letter form input[type="submit"] {
+    padding: 0 2rem;
+    font-size: 1.6rem;
+    color: #fff;
+    background: #27ae60;
+    cursor: pointer;
+}
+
+.footer .news-letter form input[type="submit"]:hover {
+    background: #130f40;
+}
+
+.footer .box-container {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+    gap: 1.5rem;
+}
+
+.footer .box-container .box h3 {
+    font-size: 2.2rem;
+    color: #130f40;
+    padding: 1rem 0;
+}
+
+.footer .box-container .box p {
+    font-size: 1.4rem;
+    color: #666;
+    padding: 1rem 0;
+}
+
+.footer .box-container .box a {
+    display: block;
+    font-size: 1.4rem;
+    color: #666;
+    padding: 1rem 0;
+}
+
+.footer .box-container .box a:hover {
+    color: #27ae60;
+}
+
+.footer .box-container .box a:hover i {
+    padding-right: 2rem;
+}
+
+.footer .box-container .box a i {
+    padding-right: .5rem;
+    color: #27ae60;
+}
+
+.footer .bottom {
+    padding-top: 1rem;
+    text-align: center;
+}
+
+.footer .bottom .share {
+    margin: 1.5rem 0;
+}
+
+.footer .bottom .share a {
+    height: 4.5rem;
+    width: 4.5rem;
+    line-height: 4.5rem;
+    font-size: 2rem;
+    border-radius: .5rem;
+    margin: 0 .3rem;
+    color: #fff;
+    background: #27ae60;
+}
+
+.footer .bottom .share a:hover {
+    background: #130f40;
+}
+
+@media (max-width: 576px) {
+    .footer .box-container {
+
+        grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
+
+    }
+
+}
+</style>
---
+++ src/pages/About.vue
@@ -0,0 +1,163 @@
+<template>
+    <section class="about-section">
+        <div class="heading">
+            <span>sobre nostros</span>
+            <h3>platos de buena calidad</h3>
+        </div>
+
+        <div class="row">
+            <div class="about-content">
+                <img src="../assets/images/taco-chefcartoon.png" alt="">
+                <div class="about-content-text">
+                    <p>Tanokura le ofrece la oportunidad de saborear exquisitos platos de la gastronomía criolla e internacional, todos elaborados con una calidad excelente que refleja el sello exclusivo y original de un lugar al que, sin duda, deseará regresar.</p>
+                    <p>Los clientes tienen la opción de deleitarse con una comida en el restaurante y sumergirse en la auténtica atmósfera cubana, o bien, pueden solicitar la entrega de comida directamente a sus hogares.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="about-article">
+                <h3>la comida une a las personas</h3>
+            </div>
+        </div>
+
+        <div class="row gallery">
+            <div class="wrapper">
+                <img src="../assets/images/taco/taco-2.jpg" alt="">
+                <img src="../assets/images/taco/taco-4.jpg" alt="">
+                <img src="../assets/images/burrito/burrito-6.jpg" alt="">
+                <img src="../assets/images/burrito/burrito-2.jpg" alt="">
+                <img src="../assets/images/burrito/burrito-3.jpg" alt="">
+                <img src="../assets/images/nachos/nachos-1.jpg" alt="">
+                <img src="../assets/images/nachos/nachos-2.jpg" alt="">
+                <img src="../assets/images/nachos/nachos-3.jpg" alt="">
+                <img src="../assets/images/dessert/dessert-2.jpg" alt="">
+                <img src="../assets/images/dessert/dessert-6.jpg" alt="">
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "About",
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Satisfy&display=swap");
+
+.about-section {
+    padding: 2rem 9%;
+}
+
+.about-section .about-content {
+    display: flex;
+    width: 100%;
+
+}
+
+.about-section .about-content img {
+    background-color: rgb(249, 249, 249);
+}
+
+.about-section .about-content .about-content-text {
+    font-size: 16px;
+    padding-left: 50px;
+}
+
+.about-section .about-article {
+    margin-top: 50px;
+    padding: 10px;
+    text-align: center;
+    background-color: #27ae60;
+    width: 100%;
+}
+
+.about-section .about-article h3 {
+    font-family: 'Satisfy', cursive;
+    font-size: 32px;
+    color: white;
+}
+
+
+.about-section .gallery {
+    overflow: hidden;
+    padding: 100px;
+}
+
+.about-section .gallery .wrapper {
+    position: relative;
+    flex-grow: 1;
+    margin: auto;
+    max-width: 1200px;
+    max-height: 1200px;
+    display: grid;
+    grid-template-columns: repeat(8, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+    grid-gap: 2vmin;
+    justify-items: center;
+    align-items: center;
+}
+
+.about-section .gallery .wrapper img {
+    z-index: 1;
+    grid-column: span 2;
+    max-width: 100%;
+    margin-bottom: -52%;
+    clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
+    transform: scale(1);
+    transition: all .25s;
+}
+
+.about-section .gallery .wrapper img:nth-child(7n + 1) {
+    grid-column: 2 / span 2;
+}
+
+.about-section .gallery .wrapper img:hover {
+    z-index: 2;
+    transform: scale(2);
+}
+
+@media (max-width: 768px) {
+    .about-section .about-content img {
+        width: 250px;
+    }
+
+    .about-section .gallery .wrapper img:hover {
+        transform: scale(2.5);
+    }
+}
+
+@media (max-width: 576px) {
+    .about-section .about-content img {
+        width: inherit;
+    }
+
+    .about-section .about-content {
+        display: block;
+    }
+
+    .about-section .about-content .about-content-text {
+        padding-left: 0px;
+    }
+
+    .about-section .gallery {
+        padding: 50px 0px;
+    }
+
+    .about-section .gallery .wrapper img {
+        max-width: 100%;
+
+    }
+
+    .about-section .gallery .wrapper {
+        grid-gap: 2vmin;
+    }
+
+    .about-section .gallery .wrapper img:hover {
+        transform: scale(2);
+    }
+
+}
+</style>
